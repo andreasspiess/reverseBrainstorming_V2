726697d92f40c0a59f690344df269e02b564c4cd
spies <spiess.andi@web.de>
2022-02-28 14:54:14 +0100

    update

--- src/main/java/com/example/reverseBrainstorming_V2/Controller/HeadController.java
+++ src/main/java/com/example/reverseBrainstorming_V2/Controller/HeadController.java
@@ -4,17 +4,48 @@ import com.example.reverseBrainstorming_V2.Forms.NegativForm;
 import com.example.reverseBrainstorming_V2.Forms.PositivForm;
 import com.example.reverseBrainstorming_V2.Forms.ProblemForm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.sql.DataSource;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class HeadController {
+
+    @Bean
+    public FlywayMigrationStrategy repairFlyway() {
+        return flyway -> {
+            // repair each script's checksum
+            flyway.repair();
+            // before new migrations are executed
+            flyway.migrate();
+        };
+    }
+
+    SimpleJdbcInsert simpleJdbcInsert;
+
+    @Autowired
+    public HeadController(DataSource dataSource) {
+        simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName("problem").usingGeneratedKeyColumns("id");
+    }
+
+    public long insertProblem(String problemText) {
+        Map<String, Object> parameters = new HashMap<>(1);
+        parameters.put("description", problemText);
+        Number newId = simpleJdbcInsert.executeAndReturnKey(parameters);
+        return (Integer) newId;
+    }
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -35,6 +66,7 @@ public class HeadController {
     public String saveProblem(Model model, ProblemForm problemForm) {
 
         jdbcTemplate.update("INSERT INTO PROBLEM VALUES(?, ?)", problemForm.getId(), problemForm.getProblem());
+        long problem_id = insertProblem(problemForm.getProblem());
 
         model.addAttribute("saveProblem", new ProblemForm());
         problem = String.valueOf(problemForm);
@@ -44,6 +76,10 @@ public class HeadController {
 
     @GetMapping("stepTwo")
     public String getNegativIdeas (Model model, NegativForm negativForm, ProblemForm problemForm) {
+        NegativForm form = new NegativForm();
+
+        form.setProblem_id(1);
+
         model.addAttribute("saveNegativIdeas", new NegativForm());
         model.addAttribute("problem", problem.toString());
         return "stepTwo";
@@ -53,6 +89,7 @@ public class HeadController {
     public String saveNegativIdeas (Model model, NegativForm negativForm) {
 
         jdbcTemplate.update("INSERT INTO NEGATIV VALUES(?, ?, ?)", negativForm.getId(), negativForm.getNegativ(), negativForm.getProblem_id());
+        long problem_id = insertProblem(negativForm.getNegativ());
 
         model.addAttribute("saveNegativIdeas", new NegativForm());
         model.addAttribute("problem", problem.toString());
@@ -62,7 +99,7 @@ public class HeadController {
     }
 
     @GetMapping("stepThree")
-    public String getPositvIdeas(Model model, PositivForm positivForm) {
+    public String getPositivIdeas(Model model, PositivForm positivForm) {
         model.addAttribute("saveNegativIdeas", new NegativForm());
         model.addAttribute("savePositivIdeas", new PositivForm());
         model.addAttribute("negativFormList", negativFormList);
